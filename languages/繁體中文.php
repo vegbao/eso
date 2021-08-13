aa2a9836064b31130f8b333fb6da5841032faf75
菜包 <36061682+vegbao@users.noreply.github.com>
2021-08-13 15:28:28 +0800

    Add Traditiona Chinese language.

---
+++ languages/繁體中文.php
@@ -0,0 +1,395 @@
+<?php
+// English (casual).php
+// English casual language pack.
+
+$language = array(
+
+"charset" => "utf-8",
+
+"*" => "*",
+"Dashboard" => "後台管理",
+"dateFormat" => "A h:i:s / Y年m月j日", // see https://php.net/manual/en/function.date.php for details
+"day ago" => "昨天",
+"days ago" => "%d 天前",
+"hour ago" => "1 小時前",
+"hours ago" => "%d 小時前",
+"minute ago" => "幾分鐘前",
+"minutes ago" => "%d 分鐘前",
+"month ago" => "1 個月前",
+"months ago" => "%d 個月前",
+"second ago" => "1 秒前",
+"seconds ago" => "%d 秒前",
+"week ago" => "上星期",
+"weeks ago" => "%d 星期前",
+"year ago" => "去年",
+"years ago" => "%d 年前",
+"a private conversation" => "私人話題",
+"Account information" => "帳號資訊",
+"Add a new plugin" => "新增插件",
+"Add a new skin" => "新增風格",
+"Add plugin" => "新增插件",
+"Add member" => "新增",
+"Add skin" => "新增風格",
+"Administrator" => "管理員",
+"Administrator-plural" => "管理員",
+"Appearance settings" => "外觀設定",
+"Are you human" => "你是人類嗎？",
+"author" => "by %s",
+"Basic settings" => "基本設定",
+"Bold" => "粗體",
+"Cancel" => "取消",
+"Can't make it out" => "看不出來？<a href='%s'>換張試試看！</a>",
+"Change avatar" => "修改頭像",
+"Change icon" => "修改圖示",
+"Change logo" => "修改標誌",
+"Change my password or email" => "修改我的密碼或信箱",
+"Change password" => "修改密碼",
+"Check for updates" => "檢查更新",
+"Confirm password" => "確認密碼",
+"Conversation" => "話題",
+"conversations" => "話題",
+"Conversations participated in" => "參與的話題",
+"Conversations started" => "話題開始",
+"Current icon" => "目前圖示",
+"Current logo" => "目前標誌",
+"Dashboard" => "後台管理",
+"Debug information" => "除錯資訊",
+"Default forum language" => "預設論壇語言",
+"delete" => "刪除",
+"Delete conversation" => "刪除話題",
+"deleted by" => "刪除自 %s",
+"disableJSEffects" => "關閉 JavaScript 效果和動畫",
+"Discard draft" => "丟棄草稿",
+"Display avatars" => "顯示頭像",
+"do not display avatars" => "不要顯示頭像",
+"edit" => "編輯",
+"edited by" => "編輯自",
+"Email" => "信箱",
+"emailOnPrivateAdd" => "當我被加入私人話題時 Email 通知我",
+"emailOnStar" => "當我開始的話題有人回覆時 Email 通知我",
+"Enter a conversation title" => "輸入話題標題",
+"Enter the web address of a logo" => "輸入標誌網址",
+"Enter the web address of an avatar" => "輸入頭像網址",
+"Enter the web address of an icon" => "輸入圖示網址",
+"Enter your email" => "輸入你的信箱",
+"Everyone" => "所有人",
+"exampleTags" => "例如：電影,冬季奧運,料理",
+"Fatal error" => "喔不！這是個糟糕的錯誤…",
+"fatalErrorMessage" => "<p>你的論壇發生了讓人討厭的錯誤導致你沒辦法繼續做你正在做的事。但別難過，<strong>你可以試著做以下幾件事情</strong>：</p>
+<ul>
+<li>散個步、遛個狗、喝杯咖啡… 然後 <strong><a href='javascript:window.location.reload()'>再試一次</a></strong>！</li>
+<li>如果你是論壇管理員，可以從 <a href='https://geteso.org'>geteso.org</a> <strong>獲得協助</strong>。</li>
+<li>試試搥一下電腦，我覺得有時候蠻有用的。</li>
+</ul>",
+"First" => "最舊",
+"First posted" => "首次發文",
+"Fixed" => "Fixed",
+"Forgot password" => "忘記密碼",
+"Forgot your password" => "忘記密碼了嗎？",
+"Forum description" => "論壇描述",
+"Forum language" => "論壇語言",
+"Forum icon" => "論壇圖示",
+"Forum logo" => "論壇標誌",
+"Forum title" => "論壇標題",
+"forumDescription" => "%s 是一個用於討論 %s 的網路論壇，和 %s。",
+"go to this post" => "前往這篇文章",
+"Header" => "標題",
+"hide" => "隱藏",
+"Home" => "首頁",
+"Hooked functions" => "攔截函式",
+"hour" => "小時",
+"hours" => "小時",
+"iconInfo" => "論壇圖示會出現在連結或網頁預覽旁，來作為你的論壇代表。你的圖示將自動調整尺寸為 256 x 256 像素。",
+"Image" => "圖片",
+"Installed plugins" => "已安裝插件",
+"Installed skins" => "已安裝風格",
+"Italic" => "斜體",
+"Join this forum" => "加入這個論壇！",
+"Join us" => "加入論壇",
+"Jump to last" => "跳到最後",
+"Jump to unread" => "跳到未讀",
+"Just now" => "剛剛",
+"labels" => array(
+	"sticky" => "置頂",
+	"private" => "私人",
+	"draft" => "草稿",
+	"locked" => "上鎖"
+),
+"Labels" => "屬性",
+"languagePackInfo" => "上傳語言包到 <code>languages/</code> 資料夾就會出現在這裡。",
+"Last" => "最新",
+"Last active" => "上次活動",
+"Last reply" => "最後回覆",
+"Link" => "連結",
+"Loading" => "載入中…",
+"Lock" => "上鎖",
+"Log in" => "登入",
+"Log out" => "登出",
+"logoInfo" => "在大多風格裡，你的標誌會出現在論壇上方的標題旁邊。你的標誌將重新調整尺寸為高度 32 像素。",
+"Start a conversation" => "開始一個話題",
+"Mark all conversations as read" => "標記所有話題為已讀",
+"Member" => "會員",
+"member online" => "<a href='" . makeLink("online") . "'>會員在線上</a>",
+"Member-plural" => "會員",
+"Members allowed to view this conversation" => "允許查看這個話題的人",
+"members online" => "<a href='" . makeLink("online") . "'>會員在線上</a>",
+"Moderator" => "版主",
+"Moderator-plural" => "版主",
+"My current password" => "我目前的密碼",
+"My profile" => "我的檔案",
+"My settings" => "我的設定",
+"MySQL queries" => "MySQL 查詢",
+"Never" => "從未",
+"New email" => "新信箱",
+"New password" => "新密碼",
+"Next" => "下一頁 &#155;",
+"No avatar" => "沒有頭像",
+"No preview" => "沒有預覽",
+"Number of characters" => "字數",
+"on alternating sides" => "兩邊",
+"on the left" => "左邊",
+"on the right" => "右邊",
+"online" => "線上",
+"Online members" => "線上會員",
+"optional" => "(選填)",
+"Other settings" => "其他設定",
+"Page loaded in" => "頁面載入時間稍微超過 <strong><span id='debugLoadTime'>%s</span> 秒</strong>",
+"Password" => "密碼",
+"Permalink to this post" => "文章的永久連結",
+"Plugins" => "插件",
+"Preview" => "預覽",
+"Previous" => "&#139; 上一頁",
+"profile" => "%s 的檔案",
+"Post a reply" => "發表回覆",
+"Post count" => "文章篇數",
+"posts" => "文章",
+"Posts" => "文章",
+"post per day" => "大約一天 1 篇",
+"posts per day" => "大約一天 %s 篇",
+"POST + GET + FILES information" => "POST + GET + FILES 資訊",
+"quote" => "引用",
+"Quote" => "\"引用\"",
+"Recent posts" => "最近的文章",
+"Recover password" => "給我新密碼！",
+"Remember me" => "記住我",
+"restore" => "還原",
+"RSS" => "RSS",
+"Sample captcha image" => "驗證碼圖片範例",
+"Save changes" => "儲存修改",
+"Save draft" => "儲存草稿",
+"Save post" => "儲存文章",
+"Search" => "搜尋！",
+"See the private conversations I've had" => "查看我和 %s 的私人話題",
+"SESSION + COOKIE information" => "SESSION + COOKIE 資訊",
+"settings" => "設定",
+"show" => "顯示",
+"Show another one" => "換張試試看",
+"show conversations participated in" => "來看看！",
+"show conversations started" => "看看他們！",
+"Show debug information to non-administrators" => "將除錯資訊給非管理員看",
+"Show forum description" => "顯示論壇描述",
+"Skin" => "風格",
+"Skins" => "風格",
+"Starred" => "Starred",
+"Start a conversation" => "開始一個話題",
+"Start a private conversation" => "和 %s 開始一個私人話題",
+"Started by" => "開始",
+"Starting a conversation" => "開始一個話題",
+"Sticky" => "置頂",
+"Strike" => "刪除線",
+"Submit post" => "送出文章！",
+"Suspended" => "停用",
+"Tags" => "標籤",
+"Type the letters you see" => "輸入你在圖片看到的字",
+"Unlock" => "解鎖",
+"unread" => "未讀",
+"Unstarred" => "無星號",
+"Unsticky" => "取消置頂",
+"Untitled conversation" => "未命名話題",
+"Unvalidated" => "無效的",
+"Update debug information for background AJAX requests" => "從背景的 AJAX 請求更新除錯資訊",
+"Upload an avatar" => "從電腦上傳頭像",
+"Upload a logo from your computer" => "從電腦上傳標誌",
+"Upload an icon from your computer" => "從電腦上傳圖示",
+"Upload a plugin" => "上傳插件包",
+"Upload a skin" => "上傳風格包",
+"Use friendly URLs" => "使用友善網址",
+"useDarkMode" => "選擇性顏色反轉 (深色模式)",
+"Use default icon" => "使用預設圖示",
+"Use default logo" => "使用預設標誌",
+"Username" => "帳號",
+"version" => "version %s",
+"Video" => "影片",
+"Viewing" => "正在看：",
+"viewingPosts" => "<b>%s-%s</b> 共 %s 篇文章",
+
+"emails" => array(
+
+"forgotPassword" => array(
+"subject" => "%s，你是不是忘記密碼啦？",
+"body" => "%s，某個人（希望是你！）送出了你在 %s 忘記密碼的請求。如果你不想修改密碼請直接忽略這封信，什麼事也不會發生。\n\n然而，如果你的確忘了密碼並想重設一個新的，請瀏覽下列連結：\n%s"),
+
+"join" => array(
+"subject" => "%s，請驗證你的帳號",
+"body" => "%s，某個人（希望是你！）用這個信箱註冊了 %s。\n\n如果是你，只要點擊下列網址就能啟用你的帳號：\n%s"),
+
+"privateAdd" => array(
+"subject" => "%s，你已被加入一個私人話題",
+"body" => "%s，你已經被加入一個叫 '%s' 的私人話題。\n\n點擊下列網址以查看這個話題：\n%s"),
+
+"newReply" => array(
+"subject" => "%s，'%s' 有新的回覆",
+"body" => "%s，%s 回覆了你開始的話題：'%s'。\n\n點擊下列網址以查看這個回覆：\n%s")
+),
+
+"confirmLeave" => "等等！你還沒把你在編輯的東存起來！一旦離開了這個頁面將會失去你所做的一切。沒問題嗎？",
+"confirmDiscard" => "你還沒把回覆存成草稿，你想放棄它嗎？",
+"confirmDeleteConversation" => "你真的確定要刪掉這個話題嗎？說真的，你沒辦法讓它回來喔！",
+"ajaxRequestPending" => "嘿！我們還在處理一些你的東西！如果你直接離開這個頁面可能會失去一些你剛才做的修改，所以再稍等幾秒就好，OK?",
+"ajaxDisconnected" => "無法連線到伺服器。稍後幾秒再 <a href='javascript:Ajax.resumeAfterDisconnection()'>重試一次</a>，或 <a href='' onclick='window.location.reload();return false'>重新整理頁面</a>。",
+);
+
+$language["gambits"] = array(
+
+// Translating the gambit system can be quite complex, but we'll do our best to get you through it. :)
+// Note: Don't use any html entities in these definitions, except for: &lt; &gt; &amp; &#39; 
+
+// Simple gambits
+// These gambits are pretty much evaluated as-they-are.
+// tag:, author:, contributor:, and quoted: are combined with a value after the colon (:).
+// For example: tag:video games, author:myself
+"tag:" => "標籤：",
+"author:" => "作者：",
+"contributor:" => "貢獻者：",
+"member" => "會員",
+"myself" => "我自己",
+"draft" => "草稿",
+"has attachments" => "含附件",
+"locked" => "上鎖",
+"order by newest" => "依最新排序",
+"order by posts" => "依回覆排序",
+"private" => "私人",
+"random" => "隨機",
+"reverse" => "舊到新",
+"starred" => "星號",
+"sticky" => "置頂",
+"unread" => "未讀",
+"more results" => "更多結果",
+
+// Aliases
+// These are gambits which tell the gambit system to use another gambit.
+// In other words, when you type "active today", the gambit system interprets it as if you typed "active 1 day".
+// The first of each pair, the alias, can be anything you want.
+// The second, however, must fit with the regular expression pattern defined below (more on that later.)
+"active today" => "今日熱門", // what appears in the gambit cloud
+"active 1 day" => "熱門 1 天", // what it actually evaluates to
+
+"has replies" => "有回覆",
+"has &gt; 1 post" => "有 &gt; 1 回覆",
+
+"has no replies" => "沒有回覆",
+"has 0 posts" => "有 0 回覆",
+
+"dead" => "死透",
+"active &gt; 30 天" => "熱門 &gt; 30 天",
+
+// Units of time
+// These are used in the active gambit.
+// ex. "[active] [>|<|>=|<=|last] 180 [second|minute|hour|day|week|month|year]"
+"second" => "秒",
+"minute" => "分鐘",
+"hour" => "小時",
+"day" => "天",
+"week" => "星期",
+"month" => "個月",
+"year" => "年",
+"last" => "最近", // as in "active last 180 days"
+"active" => "熱門" // as in "active last 180 days"
+);
+
+$language["gambits"] += array(
+
+// Now the hard bit. This is a regular expression to test for the "active" gambit.
+// The group (?<a> ... ) is the comparison operator (>, <, >=, <=, or last).
+// The group (?<b> ... ) is the number (ex. 24).
+// The group (?<c> ... ) is the unit of time.
+// The languages of "last" and the units of time are defined above.
+// However, if you need to reorder the groups, do so carefully, and make sure spaces are written as " *".
+"gambitActive" => "/^(?<a>&gt;|&lt;|&gt;=|&lt;=|{$language["gambits"]["last"]})? *(?<b>\d+) *(?<c>{$language["gambits"]["second"]}|{$language["gambits"]["minute"]}|{$language["gambits"]["hour"]}|{$language["gambits"]["day"]}|{$language["gambits"]["week"]}|{$language["gambits"]["month"]}|{$language["gambits"]["year"]}) *{$language["gambits"]["active"]}/",
+
+// These appear in the tag cloud. They must fit the regular expression pattern where the ? is a number.
+// If the regular expression pattern has been reordered, these gambits must also be reordered (as well as the ones in aliases.)
+"active last ? hours" => "{$language["gambits"]["last"]} ? {$language["gambits"]["hour"]}{$language["gambits"]["active"]}",
+"active last ? days" => "{$language["gambits"]["last"]} ? {$language["gambits"]["day"]}{$language["gambits"]["active"]}",
+
+// This is similar to the regular expression for the active gambit, but for the "has n post(s)" gambit.
+// Usually you just need to change the "has" and "post".
+"gambitHasNPosts" => "/^回覆 *(?<a>&gt;|&lt;|&gt;=|&lt;=)? *(?<b>\d+)/",
+
+// This goes by the same rules as "active last ? hours" and "active last ? days".
+"has &gt;10 posts" => "回覆 &gt;10"
+
+);
+
+$messages = array(
+"accountNotYetVerified" => array("class" => "info", "message" => "你必須先驗證帳號才能用它登入！若你沒有收到驗證信， <a href='%s'>點擊這裡再重寄一次</a>。"),
+"accountValidated" => array("class" => "success", "message" => "酷斃了！你的帳號已成功驗證，現在你可以開始參與話題了。何不自己先 <a href='" . makeLink("new") . "'>開始一個</a> 話題呢？"),
+"avatarError" => array("class" => "warning", "message" => "上傳頭像時出了差錯。請確定你上傳的是有效的圖片類型 (像 .jpg, .png, 或 .gif) 還有檔案沒有非常非常大。"),
+"beenLoggedOut" => array("class" => "warning", "message" => "糟糕！你在載入頁面時可能已被 <strong>登出</strong>。請在下方重新輸入你的密碼或是按下 <strong>取消</strong> 以忽略這個訊息。<br/><br/>輸入 <strong>%s</strong> 的密碼：%s"),
+"cannotViewConversation" => array("class" => "warning", "message" => "基於某些原因無法瀏覽這個話題。也許是被刪掉了？又或者是私人話題，在這個情況下你可能還沒登入或是你沒被邀請進去。喔天啊，我希望他們沒有在你背後偷偷說你的壞話！"),
+"captchaError" => array("class" => "warning", "message" => "糟糕！你用錯了！用這個組合再試一次。"),
+"changesSaved" => array("class" => "success", "message" => "您的修改已儲存。"),
+"conversationDeleted" => array("class" => "success", "message" => "話題已刪除。感覺不錯吧？"),
+"downloadPlugins" => array("class" => "info", "message" => "你可以從 <a href='https://github.com/geteso'>eso organization</a> 下載更多插件。"),
+"downloadSkins" => array("class" => "info", "message" => "你可以從 <a href='https://github.com/geteso'>eso organization</a> 下載更多風格。"),
+"emailDoesntExist" => array("class" => "warning", "message" => "資料庫裡找不到任何用這個信箱的會員，你是不是打錯了？"),
+"emailInfo" => array("class" => "info", "message" => "用於驗證帳號和訂閱話題。"),
+"emailTaken" => array("class" => "warning", "message" => "見鬼了，已經有會員在用這個信箱了耶！"),
+"emptyPost" => array("class" => "warning", "message" => "這個嘛…呃，你或許該在文章裡寫點什麼。"),
+"emptyTitle" => array("class" => "warning", "message" => "你的話題標題不能是空的。我是說，怎麼會有人點一個空的標題？懂我意思吧。"),
+"forgotPassword" => array("class" => "info", "message" => "如果你忘了密碼，我們會寄重設新密碼的連結給你。我們需要你的信箱。"),
+"forumDescriptionError" => array("class" => "warning", "message" => "你的論壇描述不能是空的，確保它最少要有一個字元。"),
+"forumTitleError" => array("class" => "warning", "message" => "你的論壇標題不能是空的，確保它最少要有一個字元。"),
+"gdNoSupportOpenType" => array("class" => "warning", "message" => "你安裝的 GD 版本不支援 OpenType 的字體顯示。"),
+"gdNotInstalled" => array("class" => "warning", "message" => "GD 尚未安裝。"),
+"incorrectLogin" => array("class" => "warning", "message" => "登入資訊不正確。<a href='" . makeLink("forgot-password") . "'>你是不是忘記密碼了？</a>"),
+"incorrectPassword" => array("class" => "warning", "message" => "你目前的密碼不正確。"),
+"invalidCharacters" => array("class" => "warning", "message" => "你不能在名字裡用無效的字。"),
+"invalidEmail" => array("class" => "warning", "message" => "這個信箱地址看來不太對…"),
+"invalidPlugin" => array("class" => "warning", "message" => "你上傳的插件是無效的。"),
+"invalidSkin" => array("class" => "warning", "message" => "你上傳的風格是無效的。"),
+"locked" => array("class" => "warning", "message" => "嗯…看來這個話題已經被 <strong>上鎖了</strong>，所以你無法回覆它。"),
+"lockedButCanReply" => array("class" => "info", "message" => "這個話題已經被 <strong>上鎖</strong>，但你仍然可以回覆因為你 <strong>太酷了</strong>。(還有因為你是版主或管理員。)"),
+"loginRequired" => array("class" => "warning", "message" => "嘿！你得先 <a href='" . curLink() . "#' onclick='showLogin();return false'>登入</a> 或 <a href='" . makeLink("join") . "'>註冊帳號</a> 才能使用論壇。"),
+"memberDoesntExist" => array("class" => "warning", "message" => "嗯…這裡看起來沒有用這個名字的會員。"),
+"nameEmpty" => array("class" => "warning", "message" => "你必須輸入一個名字！"),
+"nameTaken" => array("class" => "warning", "message" => "你輸入的名字已經有人使用或是含有保留字。"),
+"newSearchResults" => array("class" => "info", "message" => "有新的熱門話題更新了你的搜尋結果。<a href='javascript:Search.showNewActivity()'>顯示新的熱門話題</a>"),
+"noMembersOnline" => array("class" => "warning", "message" => "目前沒有會員在線上。"),
+"noPermission" => array("class" => "warning", "message" => "小壞蛋！你沒有權限進行這項操作。"),
+"noPluginsInstalled" => array("class" => "warning", "message" => "目前還沒有安裝插件。"),
+"noSearchResults" => array("class" => "warning", "message" => "找不到符合搜尋條件的話題。"),
+"noSkinsInstalled" => array("class" => "warning", "message" => "目前還沒有安裝風格。"),
+"notWritable" => array("class" => "warning", "message" => "<code>%s</code> 無法寫入。試著修改權限 <code>chmod</code> 為 <code>777</code>。假如它不存在，可以修改 <code>chmod</code> 它所在資料夾的權限。"),
+"passwordChanged" => array("class" => "success", "message" => "好了，你的密碼已修改。現在你可以登入了！嘿，試著別再忘了它，好嗎？"),
+"passwordEmailSent" => array("class" => "success", "message" => "好的，我們已經把重設密碼的連結寄到你的信箱了。如果接下來一兩分鐘還是沒有收到可以檢查一下垃圾郵件。嗯哼，我們有時候會被丟到垃圾郵件裡，很扯吧？！"),
+"passwordInfo" => array("class" => "info", "message" => "選擇一個安全的密碼至少要有 {$config["minPasswordLength"]} 字元。"),
+"passwordsDontMatch" => array("class" => "warning", "message" => "你的密碼並不相同。"),
+"passwordTooShort" => array("class" => "warning", "message" => "你的密碼必須至少要有 {$config["minPasswordLength"]} 字元。"),
+"pluginAdded" => array("class" => "success", "message" => "插件新增成功！"),
+"pluginCannotBeEnabled" => array("class" => "warning", "message" => "無法啟用 <em>%s</em> 插件：%s"),
+"postTooLong" => array("class" => "warning", "message" => "你的文章真的、真的、太長了！最多字數限制是 " . number_format($config["maxCharsPerPost"]) . "。那已經很長了耶！"),
+"reenterInformation" => array("class" => "info", "message" => "請重新輸入這個訊息。"),
+"removeDirectoryWarning" => array("class" => "warning", "message" => "嘿！看來你還沒刪掉我們叫你刪的那個 <code>%s</code> 資料夾！你該刪的，因為這樣會讓你的論壇安全一點。"),
+"setNewPassword" => array("class" => "info", "message" => "好來！現在你希望你的新密碼是什麼？"),
+"skinAdded" => array("class" => "success", "message" => "風格新增成功！"),
+"suspended" => array("class" => "warning", "message" => "喔不！論壇版主 <strong>停用了</strong> 你的帳號。太爛了，但在取消停用前你在這裡也不能做其他事了。哼！他算哪根蔥啊！"),
+// "updatesAvailable" => array("class" => "info", "message" => "A new version of eso (<strong>%s</strong>) is available for download. You have version {$versions["eso"]}. <strong><a href='https://github.com/geteso'>Get it now</a></strong>!"),
+"verifyEmail" => array("class" => "success", "message" => "在你開始使用新創的帳號前，你得先驗證你的信箱。在接下來的一兩分鐘內，你會收到我們寄有啟用帳號連結的信。如果這段時間都沒收到的話 <strong>檢查你的垃圾郵件</strong>！"),
+"viewMore" => array("class" => "info", "message" => "你的搜尋找到了超過 {$config["results"]} 個話題。<a href='%s' onclick='Search.viewMore();return false'>查看更多</a>"),
+"waitToReply" => array("class" => "warning", "message" => "你必須先等待至少 {$config["timeBetweenPosts"]} 秒再開始或回覆話題。先深呼吸再重試一次。"),
+"waitToSearch" => array("class" => "warning", "message" => "哇，慢一點！看來你搜尋的次數有點太頻繁了。稍等 %s 秒再試一次。"),
+
+);
+
+?>
